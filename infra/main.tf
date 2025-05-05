e79f92585f446cbb3c2f7a5e627fd6c38bde6e68
marcostdo <marcoseftoledo@gmail.com>
2025-05-05 20:40:39 -0300

    creating instance

--- infra/main.tf
+++ infra/main.tf
@@ -10,12 +10,50 @@ terraform {
 }
     
 
-
+# s3 resources
 resource "aws_s3_bucket" "mlops_bucket" {
     bucket = var.bucket_name
 
     tags = {
-        Name        = "mlops project"
+        Name        = "mlops_project"
         Environment = "prod"
     }
+}
+
+
+# ec2 resources
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+# server instance
+resource "aws_instance" "mlfloe_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mlflow"
+  }
+}
+
+# processsing instance
+resource "aws_instance" "processing_data" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "data_processing"
+  }
 }
